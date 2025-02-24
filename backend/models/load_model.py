b153f4082cf870b58c3142ea5da840b593146519
JakubSzkup1 <jakub281@hotmail.com>
2025-02-24 12:56:54 +0000

    Updated load_model.py to correctly load model

--- backend/models/load_model.py
+++ backend/models/load_model.py
@@ -1,7 +1,21 @@
+import os
 import torch
 
-def load_yolov5_model(model_path='backend/models/best.pt'):
-    # Load the YOLOv5 model
-    model = torch.hub.load('ultralytics/yolov5', 'custom', path=model_path, source='local')
-    model.eval()
-    return model
+# Absolute paths for the YOLOv5 directory and the trained model file
+YOLOV5_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "../yolov5")) # Path to YOLOv5 repo
+MODEL_PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), "best.pt")) # Path to trained model file
+
+def load_yolov5_model():
+  
+    print(f"✅ Loading YOLOv5 from: {YOLOV5_PATH}")  
+    print(f"✅ Loading model from: {MODEL_PATH}")
+
+    try:
+        model = torch.hub.load(YOLOV5_PATH, 'custom', path=MODEL_PATH, source='local', force_reload=True)    
+        model.eval()
+        print("✅ Model loaded successfully!")
+        return model
+
+    except Exception as e:
+        print(f"❌ Error loading model: {e}")
+        return None
